d85e2fa2c44b75128aed1111a82c8c55178e173a
Brendan Deere <brendan.g.deere@gmail.com>
2023-01-08 16:18:34 -0500

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { Router }  from '@vaadin/router'
+
 import 'pages/home'
+
+const router = new Router(document.getElementById('root'))
+router.setRoutes([
+  { path: '/', component: 'pages-home' }
+])
